6a5ca0ba6c5128af22bb7be498d16f5ffa8570cd
Polina Turishcheva <pturishcheva@gmail.com>
2024-07-05 11:50:40 +0000

    images time dim added

--- experanto/interpolators.py
+++ experanto/interpolators.py
@@ -21,20 +21,18 @@ class TimeInterval:
 
     def intersect(self, times):
         return (times >= self.start) & (times < self.end)
-        
-        
+
     def __repr__(self) -> str:
         return f"TimeInterval [{self.start}, {self.end})"
 
 
 class Interpolator:
-
     def __init__(self, root_folder: str) -> None:
         self.root_folder = Path(root_folder)
         meta = self.load_meta()
         self.start_time = None
         self.end_time = None
-        # Valid interval can be different to start time and end time. 
+        # Valid interval can be different to start time and end time.
         self.valid_interval = None
 
     def load_meta(self):
@@ -52,9 +50,9 @@ class Interpolator:
 
     @staticmethod
     def create(root_folder: str) -> "Interpolator":
-        with open(Path(root_folder) / 'meta.yml', 'r') as file:
+        with open(Path(root_folder) / "meta.yml", "r") as file:
             meta_data = yaml.safe_load(file)
-        modality = meta_data.get('modality')
+        modality = meta_data.get("modality")
         class_name = modality.capitalize() + "Interpolator"
         assert class_name in globals(), f"Unknown modality: {modality}"
         return globals()[class_name](root_folder)
@@ -62,21 +60,19 @@ class Interpolator:
     # CAN BE REMOVED? (DUPLICATE OF __contains__ METHOD)
     # def __contains__(self, times: np.ndarray):
     #     return np.any((times >= self.timestamps[0]) & (times <= self.timestamps[-1]))
-    
+
     def valid_times(self, times: np.ndarray) -> np.ndarray:
         return self.valid_interval.intersect(times)
 
 
-
 class SequenceInterpolator(Interpolator):
-
     def __init__(self, root_folder: str) -> None:
         super().__init__(root_folder)
         meta = self.load_meta()
-        self.time_delta = 1./meta["sampling_rate"]
+        self.time_delta = 1.0 / meta["sampling_rate"]
         self.start_time = meta["start_time"]
         self.end_time = meta["end_time"]
-        # Valid interval can be different to start time and end time. 
+        # Valid interval can be different to start time and end time.
         self.valid_interval = TimeInterval(self.start_time, self.end_time)
 
         self.use_phase_shifts = meta["phase_shift_per_signal"]
@@ -90,24 +86,37 @@ class SequenceInterpolator(Interpolator):
         if not meta["is_mem_mapped"]:
             self._data = np.load(self.root_folder / "data.npy")
         else:
-            self._data = np.memmap(self.root_folder / "data.mem", dtype=meta["dtype"], mode='r', shape=(meta["n_timestamps"], meta["n_signals"]))
+            self._data = np.memmap(
+                self.root_folder / "data.mem",
+                dtype=meta["dtype"],
+                mode="r",
+                shape=(meta["n_timestamps"], meta["n_signals"]),
+            )
 
     def interpolate(self, times: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
         valid = self.valid_times(times)
         valid_times = times[valid]
 
         if self.use_phase_shifts:
-            idx = np.floor((valid_times[:, np.newaxis] - self._phase_shifts[np.newaxis, :] - self.start_time) / self.time_delta).astype(int)
+            idx = np.floor(
+                (
+                    valid_times[:, np.newaxis]
+                    - self._phase_shifts[np.newaxis, :]
+                    - self.start_time
+                )
+                / self.time_delta
+            ).astype(int)
             data = np.take_along_axis(self._data, idx, axis=0)
         else:
-            idx = np.floor((valid_times - self.start_time) / self.time_delta).astype(int)
+            idx = np.floor((valid_times - self.start_time) / self.time_delta).astype(
+                int
+            )
             data = self._data[idx]
-            
+
         return data, valid
 
 
 class ScreenInterpolator(Interpolator):
-
     def __init__(self, root_folder: str) -> None:
         super().__init__(root_folder)
         self.timestamps = np.load(self.root_folder / "timestamps.npy")
@@ -119,23 +128,33 @@ class ScreenInterpolator(Interpolator):
         # create mapping from image index to file index
         self._num_frames = [t.num_frames for t in self.trials]
         self._first_frame_idx = [t.first_frame_idx for t in self.trials]
-        self._data_file_idx = np.concatenate([np.full(t.num_frames, i) for i, t in enumerate(self.trials)])
+        self._data_file_idx = np.concatenate(
+            [np.full(t.num_frames, i) for i, t in enumerate(self.trials)]
+        )
 
         # infer image size
         for m in self.trials:
             if m.image_size is not None:
                 self._image_size = m.image_size
                 break
-        assert np.all([(t.image_size == self._image_size) or (t.image_size is None) for t in self.trials]), \
-            'All files must have the same image size'
+        assert np.all(
+            [
+                (t.image_size == self._image_size) or (t.image_size is None)
+                for t in self.trials
+            ]
+        ), "All files must have the same image size"
 
     def _parse_trials(self) -> None:
         # Function to check if a file is a numbered yml file
         def is_numbered_yml(file_name):
-            return re.fullmatch(r'\d{5}\.yml', file_name) is not None
+            return re.fullmatch(r"\d{5}\.yml", file_name) is not None
 
         # Get block subfolders and sort by number
-        meta_files = [f for f in (self.root_folder / "meta").iterdir() if f.is_file() and is_numbered_yml(f.name)]
+        meta_files = [
+            f
+            for f in (self.root_folder / "meta").iterdir()
+            if f.is_file() and is_numbered_yml(f.name)
+        ]
         meta_files.sort(key=lambda f: int(os.path.splitext(f.name)[0]))
 
         self.trials = []
@@ -145,62 +164,95 @@ class ScreenInterpolator(Interpolator):
     def interpolate(self, times: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
         valid = self.valid_times(times)
         valid_times = times[valid]
-        valid_times += 1e-6 # add small offset to avoid numerical issues
+        valid_times += 1e-6  # add small offset to avoid numerical issues
 
         assert np.all(np.diff(valid_times) > 0), "Times must be sorted"
-        idx = np.searchsorted(self.timestamps, valid_times) - 1 # convert times to frame indices
-        assert np.all((idx >= 0) & (idx < len(self.timestamps))), "All times must be within the valid range"
+        idx = (
+            np.searchsorted(self.timestamps, valid_times) - 1
+        )  # convert times to frame indices
+        assert np.all(
+            (idx >= 0) & (idx < len(self.timestamps))
+        ), "All times must be within the valid range"
         data_file_idx = self._data_file_idx[idx]
-        
+
         # Go through files, load them and extract all frames
         unique_file_idx = np.unique(data_file_idx)
         out = np.zeros([len(valid_times)] + list(self._image_size))
         for u_idx in unique_file_idx:
             data = self.trials[u_idx].get_data()
+            if len(data.shape) == 2:
+                data = np.expand_dims(data, axis=0)
             idx_for_this_file = np.where(self._data_file_idx[idx] == u_idx)
-            out[idx_for_this_file] = data[idx[idx_for_this_file] - self._first_frame_idx[u_idx]]
+            out[idx_for_this_file] = data[
+                idx[idx_for_this_file] - self._first_frame_idx[u_idx]
+            ]
 
         return out, valid
-    
 
-class ScreenTrial():
-    def __init__(self, file_name: str, data: dict, image_size: tuple, first_frame_idx: int, num_frames: int) -> None:
+
+class ScreenTrial:
+    def __init__(
+        self,
+        file_name: str,
+        data: dict,
+        image_size: tuple,
+        first_frame_idx: int,
+        num_frames: int,
+    ) -> None:
         f = Path(file_name)
         self.file_name = f
         self.data_file_name = f.parent.parent / "data" / (f.stem + ".npy")
         self._data = data
-        self.modality = data.get('modality')
+        self.modality = data.get("modality")
         self.image_size = image_size
         self.first_frame_idx = first_frame_idx
         self.num_frames = num_frames
 
     @staticmethod
     def create(file_name: str) -> "ScreenTrial":
-        with open(file_name, 'r') as file:
+        with open(file_name, "r") as file:
             meta_data = yaml.safe_load(file)
-        modality = meta_data.get('modality')
+        modality = meta_data.get("modality")
         class_name = modality.capitalize() + "Trial"
         assert class_name in globals(), f"Unknown modality: {modality}"
         return globals()[class_name](file_name, meta_data)
-    
+
     def get_data(self) -> np.array:
         return np.load(self.data_file_name)
 
 
 class ImageTrial(ScreenTrial):
     def __init__(self, file_name, data) -> None:
-        super().__init__(file_name, data, tuple(data.get("image_size")), data.get("first_frame_idx"), 1)
+        super().__init__(
+            file_name,
+            data,
+            tuple(data.get("image_size")),
+            data.get("first_frame_idx"),
+            1,
+        )
 
 
 class VideoTrial(ScreenTrial):
     def __init__(self, file_name, data) -> None:
-        super().__init__(file_name, data, tuple(data.get("image_size")), data.get("first_frame_idx"), data.get("num_frames"))
+        super().__init__(
+            file_name,
+            data,
+            tuple(data.get("image_size")),
+            data.get("first_frame_idx"),
+            data.get("num_frames"),
+        )
 
 
 class BlankTrial(ScreenTrial):
     def __init__(self, file_name, data) -> None:
-        super().__init__(file_name, data, tuple(data.get("image_size")), data.get("first_frame_idx"), 1)
+        super().__init__(
+            file_name,
+            data,
+            tuple(data.get("image_size")),
+            data.get("first_frame_idx"),
+            1,
+        )
         self.fill_value = data.get("fill_value")
 
     def get_data(self) -> np.array:
-        return np.full((1, ) + self.image_size, self.fill_value)
+        return np.full((1,) + self.image_size, self.fill_value)
