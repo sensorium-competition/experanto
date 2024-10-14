2fbfe43da5864e0371dd981c57e6f060da452ffa
konstantinwilleke <konstantin.willeke@gmail.com>
2024-10-14 00:47:54 -0700

    Add and simplify dataloader script

--- experanto/dataloaders.py
+++ experanto/dataloaders.py
@@ -1,41 +1,17 @@
-from .data import ChunkDataset, DEFAULT_INTERP_CONFIG
+from typing import Any, List, Optional, Tuple
+from omegaconf import DictConfig
+from torch.utils.data import DataLoader
+
+from .datasets import ChunkDataset
 from .utils import MultiEpochsDataLoader, LongCycler
 
-def get_multisession_dataloaders(
-        paths: list[str],
-        interp_config: dict = DEFAULT_INTERP_CONFIG,
-        chunk_size: int = 32,
-        sampling_rate: int = 8,
-        tier: str = "train",
-        sample_stride: int = 4,
-        include_blanks: bool = True,
-        batch_size: int = 8,
-        num_workers: int = 1,
-        pin_memory: bool = True,
-        drop_last: bool = True,
-        shuffle: bool =True,
-        prefetch_factor: int = 1,
-):
-    """
-    Returns a dictionary of dataloaders for each session
-    """
+
+def get_multisession_dataloader(paths, config: DictConfig) -> DataLoader:
     dataloaders = {}
     for i, path in enumerate(paths):
-        dataset = ChunkDataset(
-            path,
-            interp_config=interp_config,
-            chunk_size=chunk_size,
-            sampling_rate=sampling_rate,
-            tier=tier,
-            sample_stride=sample_stride,
-            include_blanks=include_blanks,
-        )
-        dataloaders[i] = MultiEpochsDataLoader(dataset,
-                                                  batch_size=batch_size,
-                                                  shuffle=shuffle,
-                                                  num_workers=num_workers,
-                                                  pin_memory=pin_memory,
-                                                  drop_last=drop_last,
-                                                  prefetch_factor=prefetch_factor,
+        dataset_name = path.split("dynamic")[1].split("-Video")[0]
+        dataset = ChunkDataset(path, **config.dataset,)
+        dataloaders[dataset_name] = MultiEpochsDataLoader(dataset,
+                                               **config.dataloader,
                                                )
     return LongCycler(dataloaders)
