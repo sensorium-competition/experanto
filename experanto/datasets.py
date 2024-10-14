d64a8a21f3b3252bfda635371f9f2091d9cf18fd
konstantinwilleke <konstantin.willeke@gmail.com>
2024-10-14 00:47:23 -0700

    make full ChunkDataset functional

--- experanto/datasets.py
+++ experanto/datasets.py
@@ -2,14 +2,20 @@ from __future__ import annotations
 
 from collections import namedtuple
 from collections.abc import Iterable
-from functools import cached_property
 from pathlib import Path
 
 import numpy as np
+import torch
+import torchvision
 from torch.utils.data import Dataset
+from torchvision.transforms.v2 import ToTensor, Compose, Lambda
+from omegaconf import OmegaConf
+from hydra.utils import instantiate
 
+from .configs import DEFAULT_MODALITY_CONFIG
 from .experiment import Experiment
-from .interpolators import ImageTrial, VideoTrial, DEFAULT_INTERP_CONFIG
+from .interpolators import ImageTrial, VideoTrial
+
 
 class SimpleChunkedDataset(Dataset):
     def __init__(
@@ -17,7 +23,7 @@ class SimpleChunkedDataset(Dataset):
         root_folder: str,
         sampling_rate: float,
         chunk_size: int,
-        interp_config: dict = DEFAULT_INTERP_CONFIG,
+        interp_config: dict = DEFAULT_MODALITY_CONFIG,
     ) -> None:
         self.root_folder = Path(root_folder)
         self.sampling_rate = sampling_rate
@@ -57,7 +63,7 @@ class Mouse2pStaticImageDataset(Dataset):
         tier: str,
         offset: float,
         stim_duration: float,
-        interp_config: dict = DEFAULT_INTERP_CONFIG,
+        interp_config: dict = DEFAULT_MODALITY_CONFIG,
     ) -> None:
         self.root_folder = Path(root_folder)
         self.tier = tier
@@ -116,7 +122,7 @@ class Mouse2pVideoDataset(Dataset):
         cut: bool,
         add_channel: bool,
         channel_pos: int,
-        interp_config: dict = DEFAULT_INTERP_CONFIG,
+        interp_config: dict = DEFAULT_MODALITY_CONFIG,
     ) -> None:
         """
         this dataloader returns the full the video resampled to the new freq rate
@@ -246,88 +252,190 @@ class ChunkDataset(Dataset):
     def __init__(
         self,
         root_folder: str,
-        chunk_size: int,
-        tier: str,
-        interp_config: dict = DEFAULT_INTERP_CONFIG,
-        sampling_rate: float = None,
-        transforms: Iterable = None,
-        sample_stride = None,
-        include_blanks=False,
+        global_sampling_rate: None,
+        global_chunk_size: None,
+        modality_config: dict = DEFAULT_MODALITY_CONFIG,
     ) -> None:
+        """
+        The full modality config is a nested dictionary.
+        The following is an example of a modality config for a screen, responses, eye_tracker, and treadmill:
+
+        screen:
+          sampling_rate: null
+          chunk_size: null
+          valid_condition:
+            tier: test
+            stim_type: stimulus.Frame
+          offset: 0
+          sample_stride: 4
+          include_blanks: false
+          transforms:
+            ToTensor:
+              _target_: torchvision.transforms.ToTensor
+            Normalize:
+              _target_: torchvision.transforms.Normalize
+              mean: 80.0
+              std: 60.0
+            Resize:
+              _target_: torchvision.transforms.Resize
+              size:
+              - 144
+              - 256
+            CenterCrop:
+              _target_: torchvision.transforms.CenterCrop
+              size: 144
+          interpolation: {}
+        responses:
+          sampling_rate: null
+          chunk_size: null
+          offset: 0.1
+          transforms:
+            standardize: true
+          interpolation:
+            interpolation_mode: nearest_neighbor
+        eye_tracker:
+          sampling_rate: null
+          chunk_size: null
+          offset: 0
+          transforms:
+            normalize: true
+          interpolation:
+            interpolation_mode: nearest_neighbor
+        treadmill:
+          sampling_rate: null
+          chunk_size: null
+          offset: 0
+          transforms:
+            normalize: true
+          interpolation:
+            interpolation_mode: nearest_neighbor
+        """
         self.root_folder = Path(root_folder)
-        self.chunk_size = chunk_size
-        self.sampling_rate = sampling_rate
-        self.chunk_s = self.chunk_size / self.sampling_rate
-        self.tier = tier
-        self.sample_stride = sample_stride if sample_stride is not None else self.chunk_size
-        self.include_blanks = include_blanks
+        self.modality_config = instantiate(modality_config)
+        self.chunk_sizes, self.sampling_rates, self.chunk_s = {}, {}, {}
+        for device_name in self.modality_config.keys():
+            cfg = self.modality_config[device_name]
+            self.chunk_sizes[device_name] = global_chunk_size or cfg.chunk_size
+            self.sampling_rates[device_name] = global_sampling_rate or cfg.sampling_rate
+
+        self.sample_stride = self.modality_config.screen.sample_stride
         self._experiment = Experiment(
             root_folder,
-            interp_config,
+            modality_config,
         )
         self.device_names = self._experiment.device_names
         self.start_time, self.end_time = self._experiment.get_valid_range("screen")
-
         self._read_trials()
+        self.initialize_statistics()
         self._screen_sample_times = np.arange(
-            self.start_time, self.end_time, 1.0 / self.sampling_rate
+            self.start_time, self.end_time, 1.0 / self.sampling_rates["screen"]
         )
-        # get all indices of the sample times that belong to the tier
-        if self.include_blanks:
-            self.sample_time_in_tier = self._tier_sample_times[self.tier] + self._tier_sample_times["blank"]
-        else:
-            self.sample_time_in_tier = self._tier_sample_times[self.tier]
-        # check sample point if it is a valid item, such that index + chunk size must be within the tier
-        self._full_valid_sample_times = self.get_full_valid_screen_times(self.sample_time_in_tier)
-        # use only
+        # iterate over the valid condition in modality_config["screen"]["valid_condition"] to get the indices of self._screen_sample_times that meet all criteria
+        self._sample_in_meta_condition = self.get_sample_in_meta_condition()
+        self._full_valid_sample_times = self.get_full_valid_sample_times()
+        # the _valid_screen_times are the indices from which the starting points for the chunks will be taken
         self._valid_screen_times = self._full_valid_sample_times[::self.sample_stride]
+        self.transforms = self.initialize_transforms()
 
-    def _read_trials(self):
+    def _read_trials(self) -> None:
         screen = self._experiment.devices["screen"]
         self._trials = [t for t in screen.trials]
         start_idx = np.array([t.first_frame_idx for t in self._trials])
         self._start_times = screen.timestamps[start_idx]
         self._end_times = np.append(screen.timestamps[start_idx[1:]], np.inf)
-        self._tiers = [t.get_meta("tier") if t.get_meta("tier") is not None else "blank" for t in self._trials]
+        self.meta_conditions = {}
+        for k in self.modality_config.screen.valid_condition.keys():
+            self.meta_conditions[k] = [t.get_meta(k) if t.get_meta("tier") is not None else "blank" for t in self._trials]
 
-    @cached_property
-    def _tier_sample_times(self):
-        tier_sample_times = {}
-        for tier in np.unique(self._tiers):
-            tier_samples = []
-            for i, (trial_tier, start, end) in enumerate(zip(self._tiers, self._start_times, self._end_times)):
-                if trial_tier == tier:
-                    tier_samples.append(
+    def initialize_statistics(self) -> None:
+        self._statistics = {}
+        for device_name in self.device_names:
+            self._statistics[device_name] = {}
+            means = np.load(self._experiment.devices[device_name].root_folder / "meta/means.npy")
+            self._statistics[device_name]["mean"] = means.T # (n, 1) -> (1, n) for broadcasting in __get_item__
+            stds = np.load(self._experiment.devices[device_name].root_folder / "meta/stds.npy")
+            self._statistics[device_name]["std"] = stds.T # same as above
+
+    def initialize_transforms(self):
+        transforms = {}
+        for device_name in self.device_names:
+            if device_name == "screen":
+                # for the screen, we'll utilize torchvision.transforms
+                add_channel = Lambda(lambda x: torch.from_numpy(x[:, None, ...]) if len(x.shape) == 3 else torch.from_numpy(x))
+                transform_list = [v for v in self.modality_config.screen.transforms.values()]
+                transform_list.insert(0, add_channel)
+                transforms[device_name] = Compose(transform_list)
+            else:
+                if self.modality_config[device_name].transforms.get("normalize", False):
+                    f = lambda x: torch.from_numpy((x - self._statistics[device_name]["mean"]) / self._statistics[device_name]["std"])
+                elif self.modality_config[device_name].transforms.get("standardize", False):
+                    f = lambda x:torch.from_numpy(x / self._statistics[device_name]["std"])
+                else:
+                    f = lambda x: torch.from_numpy(x)
+                transforms[device_name] = Compose([torchvision.transforms.Lambda(f)])
+
+        return transforms
+
+    def get_sample_in_meta_condition(self) -> dict:
+        """
+        iterates through all
+           for example:
+              if meta_conditions = {"tier": [train,train, ...], "stim_type": [type1, type2, ...]}
+              and valid_condition = {"tier": train, "stim_type": type2}
+           then the output would be {"tier": [True, True, ...], "stim_type": [False, True, ...]}
+        """
+        sample_in_meta_condition = {}
+        for k, v in self.modality_config["screen"]["valid_condition"].items():
+            sample_in_meta = []
+            for i, (condition, start, end) in enumerate(zip(self.meta_conditions[k], self._start_times, self._end_times)):
+                if condition == v or (condition == "blank" and self.modality_config["screen"]["include_blanks"]):
+                    sample_in_meta.append(
                         (self._screen_sample_times >= start) & (self._screen_sample_times < end)
                     )
-            tier_sample_times[tier] = np.stack(tier_samples).sum(0).astype(bool)
-        return tier_sample_times
+            sample_in_meta_condition[k] = np.stack(sample_in_meta).sum(0).astype(bool)
+        return sample_in_meta_condition
 
-    def get_full_valid_screen_times(self, sample_time_in_tier):
+    def get_full_valid_sample_times(self, ) -> Iterable:
+        """
+        iterates through all sample times and checks if they are in the meta condition of interest
+        :return:
+        """
         valid_times = []
-        for i, idx in enumerate(sample_time_in_tier[:-self.chunk_size]):
-            if ((np.all(sample_time_in_tier[i: i + self.chunk_size])) & (
-                    self._screen_sample_times[i + self.chunk_size] < self.end_time)):
+        for i, _ in enumerate(self._screen_sample_times[:-self.chunk_sizes["screen"]]):
+            maybe_all_true = []
+            correct_duration = self._screen_sample_times[i + self.chunk_sizes["screen"]] < self.end_time
+            maybe_all_true.append(correct_duration)
+            for k, v in self._sample_in_meta_condition.items():
+                maybe_all_true.append(np.all(v[i: i + self.chunk_sizes["screen"]]))
+            if np.all(maybe_all_true):
                 valid_times.append(self._screen_sample_times[i])
         return np.stack(valid_times)
 
-    def shuffle_valid_screen_times(self):
+    def shuffle_valid_screen_times(self) -> None:
+        """
+        convenience function to randomly select new starting points for each chunk. Use this in training after each epoch.
+        """
         times = self._full_valid_sample_times
         self._valid_screen_times = np.sort(np.random.choice(times, size=len(times) // self.sample_stride, replace=False))
 
     def __len__(self):
         return len(self._valid_screen_times)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx) -> dict:
+        out = {}
         s = self._valid_screen_times[idx]
-        times = np.linspace(s, s + self.chunk_s, self.chunk_size)
-        data, _ = self._experiment.interpolate(times)
-        phase_shifts = self._experiment.devices["responses"]._phase_shifts
-        timestamps_neurons = (times - times.min())[:, None] + phase_shifts[None, :]
-        data["timestamps"] = timestamps_neurons
+        for device_name in self.device_names:
+            sampling_rate = self.sampling_rates[device_name]
+            chunk_size = self.chunk_sizes[device_name]
+            chunk_s = chunk_size / sampling_rate
 
-        # Hack-2: add batch dimension for screen
-        if len(data["screen"].shape) != 4:
-            data["screen"] = data["screen"][:, None, ...]
-        return data
+            times = np.linspace(s, s + chunk_s, chunk_size, endpoint=False)
+            times = times + self.modality_config[device_name].offset
+            data, _ = self._experiment.interpolate(times, device=device_name)
+            out[device_name] = self.transforms[device_name](data)
+
+        phase_shifts = self._experiment.devices["responses"]._phase_shifts
+        times_with_phase_shifts = (times - times.min())[:, None] + phase_shifts[None, :]
+        out["timestamps"] = torch.from_numpy(times_with_phase_shifts)
+        return out
 
