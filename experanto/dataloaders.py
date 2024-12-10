094f43dec52b2fa3779fb016347a1f26757047f6
konstantinwilleke <konstantin.willeke@gmail.com>
2024-12-10 10:53:40 -0800

    simplify multi-dataloader name

--- experanto/dataloaders.py
+++ experanto/dataloaders.py
@@ -9,7 +9,7 @@ from .utils import MultiEpochsDataLoader, LongCycler
 def get_multisession_dataloader(paths, config: DictConfig,) -> DataLoader:
     dataloaders = {}
     for i, path in enumerate(paths):
-        dataset_name = i
+        dataset_name = path.split("dynamic")[1].split("-Video")[0] if "dynamic" in path else f"session_{i}"
         dataset = ChunkDataset(path, **config.dataset,)
         dataloaders[dataset_name] = MultiEpochsDataLoader(dataset,
                                                **config.dataloader,
