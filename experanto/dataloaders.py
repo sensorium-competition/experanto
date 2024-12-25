cea5868315fdca2860a9425a11319016c6fe6a72
konstantinwilleke <konstantin.willeke@gmail.com>
2024-12-25 15:08:04 +0100

    extend get_multisession_dataloader to allow a separate cfg per path.

--- experanto/dataloaders.py
+++ experanto/dataloaders.py
@@ -6,12 +6,21 @@ from .datasets import ChunkDataset
 from .utils import MultiEpochsDataLoader, LongCycler
 
 
-def get_multisession_dataloader(paths, config: DictConfig,) -> DataLoader:
+def get_multisession_dataloader(paths: List[str],
+                                configs: Union[DictConfig, Dict, List[Union[DictConfig, Dict]]] = None,
+                                **kwargs) -> DataLoader:
+
+    if configs is None and "config" in kwargs:
+        configs = kwargs.pop("config")
+
+    # Convert single config to list for uniform handling
+    if isinstance(configs, (DictConfig, dict)):
+        configs = [configs] * len(paths)
+
     dataloaders = {}
-    for i, path in enumerate(paths):
+    for i, (path, cfg) in enumerate(zip(paths, configs)):
         dataset_name = f"session_{i}"
-        dataset = ChunkDataset(path, **config.dataset,)
+        dataset = ChunkDataset(path, **cfg.dataset)
         dataloaders[dataset_name] = MultiEpochsDataLoader(dataset,
-                                               **config.dataloader,
-                                               )
+                                                          **cfg.dataloader,)
     return LongCycler(dataloaders)
