0530a78456457e8956bd6c531e82f9998344d8d6
konstantinwilleke <konstantin.willeke@gmail.com>
2025-04-08 10:37:21 -0700

    re-adding all typeshints in dataloaders.

--- experanto/dataloaders.py
+++ experanto/dataloaders.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Optional, Tuple
+from typing import Any, List, Optional, Tuple, Union, Dict
 from omegaconf import DictConfig
 from torch.utils.data import DataLoader
 import numpy as np
